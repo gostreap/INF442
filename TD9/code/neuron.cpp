cedc9e65e96f271383b6a0a5746306a310a07f5c
Tristan François <tpv.francois@gmail.com>
2021-05-25 18:49:47 +0200

    end TD9

--- TD9/code/neuron.cpp
+++ TD9/code/neuron.cpp
@@ -1,236 +1,184 @@
 #include "neuron.hpp"
 #include "node.hpp"
 
-#include <random>
+#include <cassert>
 #include <chrono> // To seed the random generator
 #include <cmath>
-#include <cassert>
 #include <iostream>
+#include <random>
 
 // Vanilla constructor
 Neuron::Neuron(int _nb_dendrites)
-    : nb_dendrites(_nb_dendrites),
-      dendrites(_nb_dendrites + 1, nullptr),
-      axon(new Node()),
-      activation([](double x) -> double { return (x > 0) ? 1 : 0; }),
-      activation_der([](double x) -> double { return 0; }),
-      weights(_nb_dendrites + 1, 1)
-{
-    // Setup "bias-related elements"
-    dendrites[0] = new Node(-1);
-    weights[0] = 0;
-    initWeights();
-    count++;
+	: nb_dendrites(_nb_dendrites), dendrites(_nb_dendrites + 1, nullptr), axon(new Node()),
+	  activation([](double x) -> double { return (x > 0) ? 1 : 0; }),
+	  activation_der([](double x) -> double { return 0; }), weights(_nb_dendrites + 1, 1) {
+	// Setup "bias-related elements"
+	dendrites[0] = new Node(-1);
+	weights[0] = 0;
+	initWeights();
+	count++;
 }
 
 // Dendrites + activation function
-Neuron::Neuron(int _nb_dendrites,
-               std::vector<Node *> &_dendrites,
-               std::function<double(double)> _activation,
-               std::function<double(double)> _activation_der)
-    : nb_dendrites(_nb_dendrites),
-      dendrites(_dendrites),
-      axon(new Node()),
-      activation(_activation),
-      activation_der(_activation_der),
-      weights(_nb_dendrites + 1, 1)
-{
-    // Setup "bias-related elements"
-    dendrites.insert(dendrites.begin(), new Node(-1));
-    weights[0] = 0;
-    initWeights();
-    count++;
+Neuron::Neuron(int _nb_dendrites, std::vector<Node *> &_dendrites, std::function<double(double)> _activation,
+			   std::function<double(double)> _activation_der)
+	: nb_dendrites(_nb_dendrites), dendrites(_dendrites), axon(new Node()), activation(_activation),
+	  activation_der(_activation_der), weights(_nb_dendrites + 1, 1) {
+	// Setup "bias-related elements"
+	dendrites.insert(dendrites.begin(), new Node(-1));
+	weights[0] = 0;
+	initWeights();
+	count++;
 }
 
 // Dendrites + activation function
-Neuron::Neuron(int _nb_dendrites,
-               std::vector<Neuron *> &_neurons,
-               std::function<double(double)> _activation,
-               std::function<double(double)> _activation_der)
-    : nb_dendrites(_nb_dendrites),
-      dendrites(_nb_dendrites + 1, nullptr),
-      axon(new Node()),
-      activation(_activation),
-      activation_der(_activation_der),
-      weights(_nb_dendrites + 1, 1)
-{
-    // Setup "bias-related elements"
-    dendrites[0] = new Node(-1);
-    weights[0] = 0;
+Neuron::Neuron(int _nb_dendrites, std::vector<Neuron *> &_neurons, std::function<double(double)> _activation,
+			   std::function<double(double)> _activation_der)
+	: nb_dendrites(_nb_dendrites), dendrites(_nb_dendrites + 1, nullptr), axon(new Node()), activation(_activation),
+	  activation_der(_activation_der), weights(_nb_dendrites + 1, 1) {
+	// Setup "bias-related elements"
+	dendrites[0] = new Node(-1);
+	weights[0] = 0;
 
-    initWeights();
+	initWeights();
 
-    for (int i = 0; i < nb_dendrites; i++)
-        dendrites[i + 1] = _neurons[i]->axon;
+	for (int i = 0; i < nb_dendrites; i++)
+		dendrites[i + 1] = _neurons[i]->axon;
 
-    count++;
+	count++;
 }
 
-Neuron::~Neuron()
-{
-    delete dendrites[0];
-    delete axon;
-    count--;
+Neuron::~Neuron() {
+	delete dendrites[0];
+	delete axon;
+	count--;
 }
 
-void Neuron::initWeights()
-{
-    // We randomly initialise the weights for the dendrites,
-    // not for the bias, the weight for the bias is initialised to 0
-    // https://stats.stackexchange.com/questions/47590/what-are-good-initial-weights-in-a-neural-network
-    typedef std::chrono::high_resolution_clock myclock;
-    myclock::time_point beginning = myclock::now();
+void Neuron::initWeights() {
+	// We randomly initialise the weights for the dendrites,
+	// not for the bias, the weight for the bias is initialised to 0
+	// https://stats.stackexchange.com/questions/47590/what-are-good-initial-weights-in-a-neural-network
+	typedef std::chrono::high_resolution_clock myclock;
+	myclock::time_point beginning = myclock::now();
 
-    double upper_bound = 1.0 / std::sqrt(nb_dendrites);
-    double lower_bound = -upper_bound;
-    std::uniform_real_distribution<double>
-        unif(lower_bound, upper_bound);
-    std::default_random_engine re;
+	double upper_bound = 1.0 / std::sqrt(nb_dendrites);
+	double lower_bound = -upper_bound;
+	std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
+	std::default_random_engine re;
 
-    // obtain a seed from the timer
-    myclock::duration d = myclock::now() - beginning;
-    re.seed(d.count());
+	// obtain a seed from the timer
+	myclock::duration d = myclock::now() - beginning;
+	re.seed(d.count());
 
-    for (int i = 1; i <= nb_dendrites; i++)
-        weights[i] = unif(re);
+	for (int i = 1; i <= nb_dendrites; i++)
+		weights[i] = unif(re);
 }
 
 // Getters
-int Neuron::getNbDendrites() const
-{
-    return nb_dendrites;
+int Neuron::getNbDendrites() const { return nb_dendrites; }
+
+const Node *Neuron::getDendrite(int pos) const {
+	assert(0 <= pos && pos < nb_dendrites);
+	return dendrites[pos + 1];
 }
 
-const Node *Neuron::getDendrite(int pos) const
-{
-    assert(0 <= pos && pos < nb_dendrites);
-    return dendrites[pos + 1];
+const Node *Neuron::getAxon() const { return axon; }
+
+double Neuron::getWeight(int pos) const {
+	assert(0 <= pos && pos < nb_dendrites);
+	return weights[pos + 1];
 }
 
-const Node *Neuron::getAxon() const
-{
-    return axon;
-}
+double Neuron::getBias() const { return weights[0]; }
 
-double Neuron::getWeight(int pos) const
-{
-    assert(0 <= pos && pos < nb_dendrites);
-    return weights[pos + 1];
-}
+const Node *Neuron::getBiasDendrite() const { return dendrites[0]; }
 
-double Neuron::getBias() const
-{
-    return weights[0];
-}
+double Neuron::getLearningRate() const { return rate; }
 
-const Node *Neuron::getBiasDendrite() const
-{
-    return dendrites[0];
-}
+double Neuron::getOutputSignal() const { return axon->getSignal(); }
 
-double Neuron::getLearningRate() const
-{
-    return rate;
-}
-
-double Neuron::getOutputSignal() const 
-{
-    return axon->getSignal();
-}
-
-double Neuron::getCollectedInput() const
-{
-    return collected_input;
-}
+double Neuron::getCollectedInput() const { return collected_input; }
 
 // Setters
-void Neuron::setWeights(std::vector<double> &_weights)
-{
-    assert(_weights.size() == nb_dendrites + 1);
-    weights = _weights;
+void Neuron::setWeights(std::vector<double> &_weights) {
+	assert(_weights.size() == nb_dendrites + 1);
+	weights = _weights;
 }
 
-void Neuron::setWeight(int pos, double _weight)
-{
-    assert(0 <= pos && pos < nb_dendrites);
-    weights[pos + 1] = _weight;
+void Neuron::setWeight(int pos, double _weight) {
+	assert(0 <= pos && pos < nb_dendrites);
+	weights[pos + 1] = _weight;
 }
 
-void Neuron::setBias(double _bias)
-{
-    weights[0] = _bias;
-}
+void Neuron::setBias(double _bias) { weights[0] = _bias; }
 
-void Neuron::setLearningRate(double _rate)
-{
-    rate = _rate;
-}
+void Neuron::setLearningRate(double _rate) { rate = _rate; }
 
-void Neuron::setCollectedInput(double _collected_input) {
-    collected_input = _collected_input;
-}
+void Neuron::setCollectedInput(double _collected_input) { collected_input = _collected_input; }
 
-void Neuron::setBackValue(double back_value)
-{
-    axon->setBackValue(back_value);
-}
+void Neuron::setBackValue(double back_value) { axon->setBackValue(back_value); }
 
 // Forward propagation
-void Neuron::step()
-{
-    // TODO
-    // Collecting signals from dendrites
-    // Bias is stored in dendrites[0] and weights[0]
+void Neuron::step() {
+	// TODO
+	// Collecting signals from dendrites
+	// Bias is stored in dendrites[0] and weights[0]
 
-    // Compute and set the axon signal
+	// Compute and set the axon signal
+	collected_input = 0;
+	for (int i = 0; i < (int)dendrites.size(); i++) {
+		assert(dendrites[i] != NULL);
+		collected_input += weights[i] * dendrites[i]->getSignal();
+	}
+	axon->setSignal(activation(collected_input));
 }
 
 // Backward propagation (training)
-void Neuron::step_back()
-{
-    // TODO
-    // See slide 8 (pages 35-36) from INF 442 Lecture 9
-    // For each dendrite
+void Neuron::step_back() {
+	// TODO
+	// See slide 8 (pages 35-36) from INF 442 Lecture 9
+	// For each dendrite
 
-        // Propagate the error storing it in the dendrite's back value
+	// Propagate the error storing it in the dendrite's back value
 
-        // Update the associated weight
+	// Update the associated weight
+	double err = axon->getBackValue() * activation_der(collected_input);
+	for (size_t i = 0; i < dendrites.size(); i++) {
+		dendrites[i]->setBackValue(err * weights[i]);
+		weights[i] = weights[i] - rate * err * dendrites[i]->getSignal();
+	}
 }
 
 int Neuron::count = 0;
 
-int Neuron::getCount() {
-    return count;
-}
+int Neuron::getCount() { return count; }
 
 /********************************************************************/
 /* Auxiliary function for printing */
 
-std::ostream &operator<<(std::ostream &str, const Neuron &n)
-{
-    using namespace std;
+std::ostream &operator<<(std::ostream &str, const Neuron &n) {
+	using namespace std;
 
-    int count = n.getNbDendrites();
+	int count = n.getNbDendrites();
 
-    str << "Weights:";
-    for (int i = 0; i < count; i++)
-        str << " " << n.getWeight(i);
-    str << endl;
+	str << "Weights:";
+	for (int i = 0; i < count; i++)
+		str << " " << n.getWeight(i);
+	str << endl;
 
-    str << "Bias: " << n.getBias() << endl;
+	str << "Bias: " << n.getBias() << endl;
 
-    str << "Dendrites:";
-    for (int i = 0; i < count; i++)
-    {
-        if (n.getDendrite(i) != nullptr)
-            str << " " << *(n.getDendrite(i));
-        else
-            str << " - ";
-    }
-    str << endl;
+	str << "Dendrites:";
+	for (int i = 0; i < count; i++) {
+		if (n.getDendrite(i) != nullptr)
+			str << " " << *(n.getDendrite(i));
+		else
+			str << " - ";
+	}
+	str << endl;
 
-    str << "Bias: " << *n.getBiasDendrite() << endl;
-    str << "Axon: " << *n.getAxon() << endl;
+	str << "Bias: " << *n.getBiasDendrite() << endl;
+	str << "Axon: " << *n.getAxon() << endl;
 
-    return str;
+	return str;
 }
--- TD9/code/perceptron.cpp
+++ TD9/code/perceptron.cpp
@@ -1,210 +1,216 @@
 #include "perceptron.hpp"
 #include "activation.hpp"
-#include <iostream>
-#include <assert.h>
 #include <algorithm> // std::max, std::min
+#include <assert.h>
 #include <cmath>
+#include <iostream>
 
 OneLayerPerceptron::OneLayerPerceptron(int _dim, int _size, double _rate, double _decay,
-                                       std::function<double(double)> _activation,
-                                       std::function<double(double)> _activation_der)
-    : dim(_dim), size(_size), inputs(dim, nullptr), hidden(size, nullptr),
-      rate(_rate), decay(_decay)
-{
-    // To complete this constructor, have a look at those
-    // of the classes Node and Neuron
+									   std::function<double(double)> _activation,
+									   std::function<double(double)> _activation_der)
+	: dim(_dim), size(_size), inputs(dim, nullptr), hidden(size, nullptr), rate(_rate), decay(_decay) {
+	// To complete this constructor, have a look at those
+	// of the classes Node and Neuron
 
-    // Recall:
-    // dim - the dimension of the input (excluding already the regression column)
-    // size - the number of neurons in the hidden layer of the perceptron
+	// Recall:
+	// dim - the dimension of the input (excluding already the regression column)
+	// size - the number of neurons in the hidden layer of the perceptron
 
-    // TODO: Create nodes for the inputs
+	// TODO: Create nodes for the inputs
+	for (int i = 0; i < _dim; i++) {
+		inputs[i] = new Node();
+	}
 
-    // TODO: Create neurons for the hidden layer connecting them to the inputs
+	// // TODO: Create neurons for the hidden layer connecting them to the inputs
+	for (int i = 0; i < _size; i++) {
+		hidden[i] = new Neuron(_dim, inputs, _activation, _activation_der);
+	}
 
-    // TODO: Create the output neuron connecting it to those of the hidden layer
-    // and using identity (x |--> x) as the activation function
+	// // TODO: Create the output neuron connecting it to those of the hidden layer
+	// // and using identity (x |--> x) as the activation function
+	output = new Neuron(_size, hidden, id, id_der);
 
-    initLearningRate(_rate);
+	initLearningRate(_rate);
 }
 
-OneLayerPerceptron::~OneLayerPerceptron()
-{
-    // Complete this destructor
-    // (as a rule of thumb, everything that is 
-    //  created in a constructor must be deleted here)
-    for (auto node : inputs)
-    {
-        // TODO
-    }
+OneLayerPerceptron::~OneLayerPerceptron() {
+	// Complete this destructor
+	// (as a rule of thumb, everything that is
+	//  created in a constructor must be deleted here)
+	for (auto node : inputs) {
+		delete node;
+	}
 
-    // TODO
+	for (auto neuron : hidden) {
+		delete neuron;
+	}
+
+	delete output;
 }
 
- /*******************************\
+/*******************************\
 ( *          Getters            * )
- \*******************************/
+\*******************************/
 
-int OneLayerPerceptron::getNbNeurons()
-{
-    return hidden.size();
-}
+int OneLayerPerceptron::getNbNeurons() { return hidden.size(); }
 
-double OneLayerPerceptron::getLearningRate()
-{
-    return rate;
-}
+double OneLayerPerceptron::getLearningRate() { return rate; }
 
-double OneLayerPerceptron::getDecay()
-{
-    return decay;
-}
+double OneLayerPerceptron::getDecay() { return decay; }
 
- /*******************************\
+/*******************************\
 ( *          Setters            * )
- \*******************************/
+\*******************************/
 
-void OneLayerPerceptron::setLearningRate(double _rate)
-{
-    rate = _rate;
+void OneLayerPerceptron::setLearningRate(double _rate) {
+	rate = _rate;
 
-    for (auto neuron : hidden) {
-        assert (neuron != nullptr);
-        neuron->setLearningRate(rate);
-    }
+	for (auto neuron : hidden) {
+		assert(neuron != nullptr);
+		neuron->setLearningRate(rate);
+	}
 
-    assert (output != nullptr);
-    output->setLearningRate(rate);
+	assert(output != nullptr);
+	output->setLearningRate(rate);
 }
 
-void OneLayerPerceptron::initLearningRate(double _rate)
-{
-    epoch = 0;
-    setLearningRate(_rate);
+void OneLayerPerceptron::initLearningRate(double _rate) {
+	epoch = 0;
+	setLearningRate(_rate);
 }
 
-void OneLayerPerceptron::decayLearningRate()
-{
-    epoch++;
-    setLearningRate(rate / (1 + decay * epoch));
+void OneLayerPerceptron::decayLearningRate() {
+	epoch++;
+	setLearningRate(rate / (1 + decay * epoch));
 }
 
- /*******************************\
+/*******************************\
 ( *          Execution          * )
- \*******************************/
+\*******************************/
 
 const double sqrt3 = 1.73205080757;
 const double eps = 0.001;
 
-double OneLayerPerceptron::normalise(double val, Dataset *data, int coord)
-{
-    // https://stats.stackexchange.com/questions/47590/what-are-good-initial-weights-in-a-neural-network
-    // "If the inputs are normalized to have mean 0 and standard deviation 1..."
-    assert (0 <= coord && coord < data->getDim());
-    if (std::abs(data->getStddev(coord)) < eps ) 
-        return val;
-    else
-        return (val - data->getMean(coord))/data->getStddev(coord);
+double OneLayerPerceptron::normalise(double val, Dataset *data, int coord) {
+	// https://stats.stackexchange.com/questions/47590/what-are-good-initial-weights-in-a-neural-network
+	// "If the inputs are normalized to have mean 0 and standard deviation 1..."
+	assert(0 <= coord && coord < data->getDim());
+	if (std::abs(data->getStddev(coord)) < eps)
+		return val;
+	else
+		return (val - data->getMean(coord)) / data->getStddev(coord);
 }
 
-double OneLayerPerceptron::denormalise(double val, Dataset *data, int coord)
-{
-    assert (0 <= coord && coord < data->getDim());
-    return val*data->getStddev(coord) + data->getMean(coord);
+double OneLayerPerceptron::denormalise(double val, Dataset *data, int coord) {
+	assert(0 <= coord && coord < data->getDim());
+	return val * data->getStddev(coord) + data->getMean(coord);
 }
 
-void OneLayerPerceptron::prepareInputs(Dataset *data, int row, int regr, bool print)
-{
-    using namespace std;
+void OneLayerPerceptron::prepareInputs(Dataset *data, int row, int regr, bool print) {
+	using namespace std;
 
-    // Initialise the input signals, skipping the regression
-    // column -- do not forget to normalise the data (see above)
+	// Initialise the input signals, skipping the regression
+	// column -- do not forget to normalise the data (see above)
 
-    if (print)
-        cout << "Initialising input signals...\t";
+	if (print)
+		cout << "Initialising input signals...\t";
 
-    // TODO
+	// TODO
+	std::vector<double> instance = data->getInstance(row);
 
-    if (print)
-        cout << "done." << endl;
+	for (int i = 0; i < data->getDim(); i++) {
+		if (i < regr) {
+			inputs[i]->setSignal(normalise(instance[i], data, i));
+		} else if (i > regr) {
+			inputs[i - 1]->setSignal(normalise(instance[i], data, i));
+		}
+	}
+
+	if (print)
+		cout << "done." << endl;
 }
 
-void OneLayerPerceptron::computeHiddenStep(bool print)
-{
-    using namespace std;
+void OneLayerPerceptron::computeHiddenStep(bool print) {
+	using namespace std;
 
-    if (print)
-        cout << "Running internal layer neurons..." << endl;
+	if (print)
+		cout << "Running internal layer neurons..." << endl;
 
-    // TODO: Compute one step of the hidden layer
+	// TODO: Compute one step of the hidden layer
+	for (Neuron *n : hidden) {
+		n->step();
+	}
 
-    if (print)
-        cout << "done." << endl;
+	if (print)
+		cout << "done." << endl;
 }
 
-double OneLayerPerceptron::computeOutputStep(Dataset *data, int row, int regr, bool print)
-{
-    // This method should comprise both forward and backward propagation
-    
-    using namespace std;
+double OneLayerPerceptron::computeOutputStep(Dataset *data, int row, int regr, bool print) {
+	// This method should comprise both forward and backward propagation
 
-    double ret, error;
+	using namespace std;
 
-    if (print)
-        cout << "Running output neuron..." << endl
-             << *output << endl;
+	double ret, error;
 
-    // TODO: Compute one step of the output neuron
+	if (print)
+		cout << "Running output neuron..." << endl << *output << endl;
 
-    if (print)
-        cout << "done. Output = " << ret << endl;
+	// TODO: Compute one step of the output neuron
+	output->step();
+	ret = output->getAxon()->getSignal();
 
-    if (print)
-        cout << "Setting up back propagation...\t";
+	if (print)
+		cout << "done. Output = " << ret << endl;
 
-    // Compute the error for back propagation
-    error = -2 * (normalise(data->getInstance(row)[regr], data, regr) - ret);
-    // TODO: Initialise the back propagation
+	if (print)
+		cout << "Setting up back propagation...\t";
 
-    if (print)
-    {
-        cout << "done. Propagated error = " << error << endl;
-        cout << "Running back propagation through the output neuron...\t";
-    }
+	// Compute the error for back propagation
+	error = -2 * (normalise(data->getInstance(row)[regr], data, regr) - ret);
+	// TODO: Initialise the back propagation
 
-    // TODO: Propagate back through the output neuron
+	output->setBackValue(error);
 
-    if (print)
-        cout << "done." << endl;
+	if (print) {
+		cout << "done. Propagated error = " << error << endl;
+		cout << "Running back propagation through the output neuron...\t";
+	}
 
-    return denormalise(ret, data, regr);
+	// TODO: Propagate back through the output neuron
+	output->step_back();
+
+	if (print)
+		cout << "done." << endl;
+
+	return denormalise(ret, data, regr);
 }
 
-void OneLayerPerceptron::propagateBackHidden(bool print)
-{
-    using namespace std;
+void OneLayerPerceptron::propagateBackHidden(bool print) {
+	using namespace std;
 
-    if (print)
-        cout << "Running back propagation through the hidden layer...\t";
+	if (print)
+		cout << "Running back propagation through the hidden layer...\t";
 
-    // TODO: Propagate back through the hidden layer
+	// TODO: Propagate back through the hidden layer
+	for (Neuron *n : hidden) {
+		n->step_back();
+	}
 
-    if (print)
-        cout << "done." << endl;
+	if (print)
+		cout << "done." << endl;
 }
 
-double OneLayerPerceptron::run(Dataset *data, int row, int regr, bool print)
-{
-    assert(data->getDim() == dim + 1);
-    assert(0 <= regr && regr < data->getDim());
+double OneLayerPerceptron::run(Dataset *data, int row, int regr, bool print) {
+	assert(data->getDim() == dim + 1);
+	assert(0 <= regr && regr < data->getDim());
 
-    prepareInputs(data, row, regr, print);
+	prepareInputs(data, row, regr, print);
 
-    computeHiddenStep(print);
+	computeHiddenStep(print);
 
-    double ret = computeOutputStep(data, row, regr, print);
+	double ret = computeOutputStep(data, row, regr, print);
 
-    propagateBackHidden(print);
+	propagateBackHidden(print);
 
-    return ret;
+	return ret;
 }
